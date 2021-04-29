4368721b759fddaca348bea28c1ca6e81c0318c4
thasuohm <thasuohm@gmail.com>
2021-04-29 14:04:00 +0700

    [UPDATE] template for TeamMember

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,5 +1,66 @@
 <template>
-    <div>    
-        <h1 class= " text-6xl">Member</h1>
+    <div class= "mx-auto">    
+        <h1 class= " text-6xl header text-red-400">Our Team</h1>
+        <h1 class= " text-l description"><b>INT 221 Integrated Project</b></h1>
+        
+        
+        <div class="grid grid-cols-3 border border-black mr-28 ml-24 p-1 mt-2">
+            <img class="rounded-full w-48 h-auto row-span-3 " src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+            
+            <div class="row-span-3 col-span-2">
+            <h1 class="header text-4xl pt-4 pr-1 text-left pl-10">Thanasit Eksoragul</h1>
+            <h1 class=" text-red-400 text-left pl-10"><b>BackEnd</b></h1>
+            <h1 class=" text-left pl-10"><b>ID : 62130500036</b></h1>
+        </div>
+        </div>
+
+        <div class="grid grid-cols-3 border border-black mr-28 ml-24 p-1 mt-2">
+            <img class="rounded-full w-48 h-auto row-span-3 " src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+            
+            <div class="row-span-3 col-span-2">
+            <h1 class="header text-4xl pt-4 pr-1 text-left pl-10">Thanapat Suwannaard</h1>
+            <h1 class=" text-red-400 text-left pl-10"><b>FrontEnd</b></h1>
+            <h1 class=" text-left pl-10"><b>ID : 62130500037</b></h1>
+        </div>
+        </div>
+
+
+        
+
+
+
+        <div class="grid grid-cols-3 border border-black mr-28 ml-24 p-1 mt-2">
+            <img class="rounded-full w-48 h-auto row-span-3 " src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+            
+            <div class="row-span-3 col-span-2">
+            <h1 class="header text-3xl pt-4 pr-1 text-left pl-10">Punthanat Ularnwiriyanont</h1>
+            <h1 class="text-red-400  text-left pl-10"><b>BackEnd</b></h1>
+            <h1 class=" text-left pl-10"><b>ID : 62130500058</b></h1>
+        </div>
+        </div>
+
+
+
+
+
+
+
     </div>
 </template>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@1,300&display=swap');
+.header {
+    font-family: 'Bebas Neue', cursive;
+
+}
+
+.description{
+    font-family: 'Roboto Condensed', sans-serif;
+}
+
+
+
+</style>
