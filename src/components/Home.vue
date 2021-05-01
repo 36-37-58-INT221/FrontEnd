c4ad439480b03a3298e6bddb7410d83208fdf81f
thasuohm <thasuohm@gmail.com>
2021-05-01 12:06:27 +0700

    [UPDATE] clean home bar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,21 @@
 <template>
-    <div class=" text-left text-white text-2xl py-4 pl-2  pb-2"><h1 v-if="this.$route.path != '/'">{{ title }}</h1><h1 v-else>Home</h1></div>
+    <div class=" text-left text-white text-2xl py-4 pl-2  pb-2"> 
+        <h1 v-if="this.$route.path == '/ProductList'">Product List</h1>
+        <h1 v-else-if="this.$route.path == '/Team'">Team</h1>
+        <h1 v-else-if="this.$route.path == '/'">Home</h1>
+        <h1 v-else-if="this.$route.path == '/Addproduct'">Add Product </h1>
+    </div>
     
     
     
     <div class="bg-gray-700 text-left text-white py-2"> 
     <div class= "ml-2 hover:bg-red-500 hover:my-2   inline"> <router-link to="/">Home</router-link></div>   
-    <div class="ml-2 hover:bg-red-500  inline" @click ="goProductList()"> Product</div>   
-    <div class="ml-2 hover:bg-red-500 inline"  @click ="goTeam()">  Team</div>   
+    <div class="ml-2 hover:bg-red-500  inline"> <router-link to="/ProductList">Product</router-link></div>   
+    <div class="ml-2 hover:bg-red-500 inline"> <router-link to="/Team"> Team</router-link></div>   
     </div>
 
 </template>
 
-
-<script>
-    import router from '../router';
-export default {
-    data() {
-        return {
-            title : null,
-        }
-    },
-    methods: {
-
-        goProductList(){
-            this.title="Product List" 
-            router.push("/ProductList")
-        },
-        goTeam(){
-            this.title="Team"
-            router.push("/Team")
-        }
-    },
-    mounted(){
-
-
-
-    }
-
-
-
-
-
-
-
-}
-</script>
 
 
 
